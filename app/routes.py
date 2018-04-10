57dd4a1730bb87a7076463ed0de404688e8a681e
uzul <mhvdm03@gmail.com>
2018-04-10 22:14:47 +0300

    add core files

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = {'username': "Uzul"}
+	posts = [
+		{
+			'author': {'username': 'user1'}, 
+			'body': 'text 1'
+		}
+	]
+	return render_template('index.html', title="hhome", user=user, posts=posts)
