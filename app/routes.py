6ecb30643988747894493f12fdc626721e1449ed
uzul <mhvdm03@gmail.com>
2018-04-10 22:57:00 +0300

    change indent

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,11 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': "Uzul"}
-	posts = [
-		{
-			'author': {'username': 'user1'}, 
-			'body': 'text 1'
-		}
-	]
-	return render_template('index.html', title="hhome", user=user, posts=posts)
+    user = {'username': "Uzul"}
+    posts = [
+        {
+            'author': {'username': 'user1'},
+            'body': 'text 1'
+        }
+    ]
+    return render_template('index.html', title="hhome", user=user, posts=posts)
