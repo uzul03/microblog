3b7ba27d1fc7a0a2c43e7e0507c81984a4ab31c3
uzul <mhvdm03@gmail.com>
2018-05-06 20:16:09 +0300

    правки полей базы

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    post = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -20,4 +20,5 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return "<Post {}>".format(self.post)
+        return "<Post {}>".format(self.body)
+
