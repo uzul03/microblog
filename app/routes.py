5d48d3d066dd1ec6b90e9d181cae506d39422dee
uzul <mhvdm03@gmail.com>
2018-05-08 07:33:15 +0300

    logout

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from .forms import LoginForm
 from app.models import User
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 
 
 @app.route('/')
@@ -33,3 +33,9 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
