572b811987dc380f2523ebff5ac1b839b0ebed35
uzul <mhvdm03@gmail.com>
2018-05-09 10:45:58 +0300

    логин и перенаправление для реального пользователя

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,16 @@
 from flask import render_template, flash, redirect, url_for
+from flask import request
 from app import app
 from .forms import LoginForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user
+from flask_login import login_required
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': "Uzul"}
     posts = [
@@ -15,7 +19,7 @@ def index():
             'body': 'text 1'
         }
     ]
-    return render_template('index.html', title="hhome", user=user, posts=posts)
+    return render_template('index.html', title="Home Page", posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -26,12 +30,14 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user in None or not user.check_password(form.password.data):
+        if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password")
             return redirect(url_for('login'))
-
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
