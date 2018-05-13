130eb3684dabff65cdf517dea7a5b86b956dc96a
uzul <mhvdm03@gmail.com>
2018-05-13 13:37:14 +0300

    редактирвоание данных пользователя

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app import app
 from app import db
 from .forms import LoginForm
 from .forms import RegistrationForm
+from .forms import EditProfileForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user
 from flask_login import login_required
@@ -80,3 +81,19 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changed have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title="Edit Profile", form=form)
