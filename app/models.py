512bc88ec45a0882f4a86fd721f52b23653c22c7
uzul <mhvdm03@gmail.com>
2018-05-22 08:09:26 +0300

    запрос на сообщения followed-user

--- app/models.py
+++ app/models.py
@@ -55,6 +55,13 @@ class User(UserMixin, db.Model):
         if self.is_followed(user):
             self.followed.remove(user)
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id).order_by(
+            Post.timestamp.desc())
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
