4e85509bc69bf92a5a9fb4b0ed638089cae3982a
uzul <mhvdm03@gmail.com>
2018-05-23 08:07:52 +0300

    заготовка для unit-тестов

--- app/models.py
+++ app/models.py
@@ -63,6 +63,7 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
