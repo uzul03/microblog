3111cb22f5a449a5aa64e9b4dc3c9634678e2e79
uzul <mhvdm03@gmail.com>
2018-05-24 08:09:32 +0300

    unit-тесты для пользователей

--- app/models.py
+++ app/models.py
@@ -58,8 +58,7 @@ class User(UserMixin, db.Model):
     def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-            followers.c.follower_id == self.id).order_by(
-            Post.timestamp.desc())
+            followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
