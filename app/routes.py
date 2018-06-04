e101e53f2f36341fbd9839ad6a1d33a39908fc18
uzul <mhvdm03@gmail.com>
2018-06-04 07:17:20 +0300

    навигация по страница сообщений пользователя

--- app/routes.py
+++ app/routes.py
@@ -33,8 +33,11 @@ def index():
         return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(page, app.config['POST_PER_PAGE'], False)
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
     return render_template('index.html', title="Home Page",
-                           form=form, posts=posts.items)
+                           form=form, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/explorer')
@@ -42,7 +45,11 @@ def index():
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POST_PER_PAGE'], False)
-    return render_template('index.html', title='Explore', posts=posts.items)
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    return render_template('index.html', title='Explore',
+                           posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -89,8 +96,14 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts.filter_by().order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POST_PER_PAGE'], False)
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/edit_profile', methods=["GET", "POST"])
