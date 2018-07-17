e08db10169957f839cdb27ec7338d2b9d56bf3ee
uzul <mhvdm03@gmail.com>
2018-07-17 08:16:06 +0300

    отображение даты и времени для постов пользователей
    использование moment.js и flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 import os
 
@@ -19,6 +20,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
